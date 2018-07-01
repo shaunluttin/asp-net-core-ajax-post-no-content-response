94f45a9f295bbe799770c88cfb15dc3d369ab22a
Shaun Luttin <shaunluttin@gmail.com>
2018-07-01 15:49:05 -0700

    Add example of POST with no-content

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreAjaxOkResponse.Controllers
+{
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("api/values")]
+        public IActionResult Post() => new OkResult();
+
+        [HttpPost("api/values1")]
+        public IActionResult Post1() => new StatusCodeResult(204); 
+
+        [HttpPost("api/values2")]
+        public IActionResult Post2() => Ok("");
+
+        [HttpPost("api/values3")]
+        public IActionResult Post3() => new OkObjectResult("");
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AspNetCoreAjaxOkResponse
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseFileServer(); // index.html
+            app.UseMvc();
+        }
+    }
+}
